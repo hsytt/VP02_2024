ec18108544eb51fb7982ee3bf38281000e4eb3fa
hsytt <165130077+hsytt@users.noreply.github.com>
2024-03-27 15:30:35 +0900

    Add files via upload

---
+++ 004_bmiForms/004_bmiForms.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace _004_bmiForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblResult = new System.Windows.Forms.Label();
+            this.txtW = new System.Windows.Forms.TextBox();
+            this.lblBMI = new System.Windows.Forms.Label();
+            this.lblW = new System.Windows.Forms.Label();
+            this.txtH = new System.Windows.Forms.TextBox();
+            this.lblH = new System.Windows.Forms.Label();
+            this.btnBMI = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(153, 190);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(44, 50);
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(42, 228);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(29, 12);
+            this.lblResult.TabIndex = 14;
+            this.lblResult.Text = "판정";
+            // 
+            // txtW
+            // 
+            this.txtW.Location = new System.Drawing.Point(97, 98);
+            this.txtW.Name = "txtW";
+            this.txtW.Size = new System.Drawing.Size(100, 21);
+            this.txtW.TabIndex = 13;
+            // 
+            // lblBMI
+            // 
+            this.lblBMI.AutoSize = true;
+            this.lblBMI.Location = new System.Drawing.Point(42, 190);
+            this.lblBMI.Name = "lblBMI";
+            this.lblBMI.Size = new System.Drawing.Size(41, 12);
+            this.lblBMI.TabIndex = 12;
+            this.lblBMI.Text = "BMI = ";
+            // 
+            // lblW
+            // 
+            this.lblW.AutoSize = true;
+            this.lblW.Location = new System.Drawing.Point(42, 104);
+            this.lblW.Name = "lblW";
+            this.lblW.Size = new System.Drawing.Size(52, 12);
+            this.lblW.TabIndex = 11;
+            this.lblW.Text = "체중(kg)";
+            // 
+            // txtH
+            // 
+            this.txtH.Location = new System.Drawing.Point(97, 53);
+            this.txtH.Name = "txtH";
+            this.txtH.Size = new System.Drawing.Size(100, 21);
+            this.txtH.TabIndex = 10;
+            // 
+            // lblH
+            // 
+            this.lblH.AutoSize = true;
+            this.lblH.Location = new System.Drawing.Point(42, 56);
+            this.lblH.Name = "lblH";
+            this.lblH.Size = new System.Drawing.Size(45, 12);
+            this.lblH.TabIndex = 9;
+            this.lblH.Text = "키(cm)";
+            // 
+            // btnBMI
+            // 
+            this.btnBMI.Location = new System.Drawing.Point(122, 139);
+            this.btnBMI.Name = "btnBMI";
+            this.btnBMI.Size = new System.Drawing.Size(75, 23);
+            this.btnBMI.TabIndex = 8;
+            this.btnBMI.Text = "BMI 계산";
+            this.btnBMI.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(229, 302);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.txtW);
+            this.Controls.Add(this.lblBMI);
+            this.Controls.Add(this.lblW);
+            this.Controls.Add(this.txtH);
+            this.Controls.Add(this.lblH);
+            this.Controls.Add(this.btnBMI);
+            this.Name = "Form1";
+            this.Text = "BMI 계산기";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.TextBox txtW;
+        private System.Windows.Forms.Label lblBMI;
+        private System.Windows.Forms.Label lblW;
+        private System.Windows.Forms.TextBox txtH;
+        private System.Windows.Forms.Label lblH;
+        private System.Windows.Forms.Button btnBMI;
+    }
+}
+
---
+++ 005_Login/005_Login.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace _005_Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.txtPW = new System.Windows.Forms.TextBox();
+            this.lblPW = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtResult
+            // 
+            this.txtResult.BackColor = System.Drawing.SystemColors.Control;
+            this.txtResult.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtResult.Location = new System.Drawing.Point(112, 165);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.ReadOnly = true;
+            this.txtResult.Size = new System.Drawing.Size(120, 21);
+            this.txtResult.TabIndex = 13;
+            // 
+            // txtPW
+            // 
+            this.txtPW.Location = new System.Drawing.Point(112, 86);
+            this.txtPW.Name = "txtPW";
+            this.txtPW.PasswordChar = '●';
+            this.txtPW.Size = new System.Drawing.Size(120, 21);
+            this.txtPW.TabIndex = 12;
+            // 
+            // lblPW
+            // 
+            this.lblPW.AutoSize = true;
+            this.lblPW.Location = new System.Drawing.Point(40, 89);
+            this.lblPW.Name = "lblPW";
+            this.lblPW.Size = new System.Drawing.Size(53, 12);
+            this.lblPW.TabIndex = 11;
+            this.lblPW.Text = "패스워드";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(112, 49);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(120, 21);
+            this.txtID.TabIndex = 10;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(52, 53);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(41, 12);
+            this.lblID.TabIndex = 9;
+            this.lblID.Text = "아이디";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(163, 126);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(69, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 276);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtPW);
+            this.Controls.Add(this.lblPW);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Login";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtResult;
+        private System.Windows.Forms.TextBox txtPW;
+        private System.Windows.Forms.Label lblPW;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
