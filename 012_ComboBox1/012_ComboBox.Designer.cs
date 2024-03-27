fef2c816125110698968c9f7aa4a615f4a438c6e
hsytt <165130077+hsytt@users.noreply.github.com>
2024-03-27 15:32:45 +0900

    Add files via upload

---
+++ 012_ComboBox1/012_ComboBox.Designer.cs
@@ -0,0 +1,366 @@
+﻿namespace _012_ComboBox1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.grd8 = new System.Windows.Forms.ComboBox();
+            this.grd7 = new System.Windows.Forms.ComboBox();
+            this.grd6 = new System.Windows.Forms.ComboBox();
+            this.grd5 = new System.Windows.Forms.ComboBox();
+            this.grd4 = new System.Windows.Forms.ComboBox();
+            this.grd3 = new System.Windows.Forms.ComboBox();
+            this.grd2 = new System.Windows.Forms.ComboBox();
+            this.grd1 = new System.Windows.Forms.ComboBox();
+            this.lblGrade = new System.Windows.Forms.Label();
+            this.crd8 = new System.Windows.Forms.ComboBox();
+            this.crd7 = new System.Windows.Forms.ComboBox();
+            this.crd6 = new System.Windows.Forms.ComboBox();
+            this.crd5 = new System.Windows.Forms.ComboBox();
+            this.crd4 = new System.Windows.Forms.ComboBox();
+            this.crd3 = new System.Windows.Forms.ComboBox();
+            this.crd2 = new System.Windows.Forms.ComboBox();
+            this.crd1 = new System.Windows.Forms.ComboBox();
+            this.lblCrd = new System.Windows.Forms.Label();
+            this.txt7 = new System.Windows.Forms.TextBox();
+            this.txt6 = new System.Windows.Forms.TextBox();
+            this.txt5 = new System.Windows.Forms.TextBox();
+            this.txt4 = new System.Windows.Forms.TextBox();
+            this.txt8 = new System.Windows.Forms.TextBox();
+            this.txt3 = new System.Windows.Forms.TextBox();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Location = new System.Drawing.Point(328, 297);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.ReadOnly = true;
+            this.txtGrade.Size = new System.Drawing.Size(100, 21);
+            this.txtGrade.TabIndex = 57;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(232, 297);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(86, 23);
+            this.button1.TabIndex = 56;
+            this.button1.Text = "평균평점";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // grd8
+            // 
+            this.grd8.FormattingEnabled = true;
+            this.grd8.Location = new System.Drawing.Point(307, 255);
+            this.grd8.Name = "grd8";
+            this.grd8.Size = new System.Drawing.Size(121, 20);
+            this.grd8.TabIndex = 55;
+            // 
+            // grd7
+            // 
+            this.grd7.FormattingEnabled = true;
+            this.grd7.Location = new System.Drawing.Point(307, 228);
+            this.grd7.Name = "grd7";
+            this.grd7.Size = new System.Drawing.Size(121, 20);
+            this.grd7.TabIndex = 54;
+            // 
+            // grd6
+            // 
+            this.grd6.FormattingEnabled = true;
+            this.grd6.Location = new System.Drawing.Point(307, 202);
+            this.grd6.Name = "grd6";
+            this.grd6.Size = new System.Drawing.Size(121, 20);
+            this.grd6.TabIndex = 53;
+            // 
+            // grd5
+            // 
+            this.grd5.FormattingEnabled = true;
+            this.grd5.Location = new System.Drawing.Point(307, 175);
+            this.grd5.Name = "grd5";
+            this.grd5.Size = new System.Drawing.Size(121, 20);
+            this.grd5.TabIndex = 52;
+            // 
+            // grd4
+            // 
+            this.grd4.FormattingEnabled = true;
+            this.grd4.Location = new System.Drawing.Point(307, 148);
+            this.grd4.Name = "grd4";
+            this.grd4.Size = new System.Drawing.Size(121, 20);
+            this.grd4.TabIndex = 51;
+            // 
+            // grd3
+            // 
+            this.grd3.FormattingEnabled = true;
+            this.grd3.Location = new System.Drawing.Point(307, 121);
+            this.grd3.Name = "grd3";
+            this.grd3.Size = new System.Drawing.Size(121, 20);
+            this.grd3.TabIndex = 50;
+            // 
+            // grd2
+            // 
+            this.grd2.FormattingEnabled = true;
+            this.grd2.Location = new System.Drawing.Point(307, 95);
+            this.grd2.Name = "grd2";
+            this.grd2.Size = new System.Drawing.Size(121, 20);
+            this.grd2.TabIndex = 49;
+            // 
+            // grd1
+            // 
+            this.grd1.FormattingEnabled = true;
+            this.grd1.Location = new System.Drawing.Point(307, 68);
+            this.grd1.Name = "grd1";
+            this.grd1.Size = new System.Drawing.Size(121, 20);
+            this.grd1.TabIndex = 48;
+            // 
+            // lblGrade
+            // 
+            this.lblGrade.AutoSize = true;
+            this.lblGrade.Location = new System.Drawing.Point(348, 37);
+            this.lblGrade.Name = "lblGrade";
+            this.lblGrade.Size = new System.Drawing.Size(29, 12);
+            this.lblGrade.TabIndex = 47;
+            this.lblGrade.Text = "성적";
+            // 
+            // crd8
+            // 
+            this.crd8.FormattingEnabled = true;
+            this.crd8.Location = new System.Drawing.Point(157, 255);
+            this.crd8.Name = "crd8";
+            this.crd8.Size = new System.Drawing.Size(121, 20);
+            this.crd8.TabIndex = 46;
+            // 
+            // crd7
+            // 
+            this.crd7.FormattingEnabled = true;
+            this.crd7.Location = new System.Drawing.Point(157, 228);
+            this.crd7.Name = "crd7";
+            this.crd7.Size = new System.Drawing.Size(121, 20);
+            this.crd7.TabIndex = 45;
+            // 
+            // crd6
+            // 
+            this.crd6.FormattingEnabled = true;
+            this.crd6.Location = new System.Drawing.Point(157, 202);
+            this.crd6.Name = "crd6";
+            this.crd6.Size = new System.Drawing.Size(121, 20);
+            this.crd6.TabIndex = 44;
+            // 
+            // crd5
+            // 
+            this.crd5.FormattingEnabled = true;
+            this.crd5.Location = new System.Drawing.Point(157, 175);
+            this.crd5.Name = "crd5";
+            this.crd5.Size = new System.Drawing.Size(121, 20);
+            this.crd5.TabIndex = 43;
+            // 
+            // crd4
+            // 
+            this.crd4.FormattingEnabled = true;
+            this.crd4.Location = new System.Drawing.Point(157, 148);
+            this.crd4.Name = "crd4";
+            this.crd4.Size = new System.Drawing.Size(121, 20);
+            this.crd4.TabIndex = 42;
+            // 
+            // crd3
+            // 
+            this.crd3.FormattingEnabled = true;
+            this.crd3.Location = new System.Drawing.Point(157, 121);
+            this.crd3.Name = "crd3";
+            this.crd3.Size = new System.Drawing.Size(121, 20);
+            this.crd3.TabIndex = 41;
+            // 
+            // crd2
+            // 
+            this.crd2.FormattingEnabled = true;
+            this.crd2.Location = new System.Drawing.Point(157, 95);
+            this.crd2.Name = "crd2";
+            this.crd2.Size = new System.Drawing.Size(121, 20);
+            this.crd2.TabIndex = 40;
+            // 
+            // crd1
+            // 
+            this.crd1.FormattingEnabled = true;
+            this.crd1.Location = new System.Drawing.Point(157, 68);
+            this.crd1.Name = "crd1";
+            this.crd1.Size = new System.Drawing.Size(121, 20);
+            this.crd1.TabIndex = 39;
+            // 
+            // lblCrd
+            // 
+            this.lblCrd.AutoSize = true;
+            this.lblCrd.Location = new System.Drawing.Point(198, 37);
+            this.lblCrd.Name = "lblCrd";
+            this.lblCrd.Size = new System.Drawing.Size(29, 12);
+            this.lblCrd.TabIndex = 38;
+            this.lblCrd.Text = "학점";
+            // 
+            // txt7
+            // 
+            this.txt7.Location = new System.Drawing.Point(28, 229);
+            this.txt7.Name = "txt7";
+            this.txt7.Size = new System.Drawing.Size(100, 21);
+            this.txt7.TabIndex = 37;
+            // 
+            // txt6
+            // 
+            this.txt6.Location = new System.Drawing.Point(28, 202);
+            this.txt6.Name = "txt6";
+            this.txt6.Size = new System.Drawing.Size(100, 21);
+            this.txt6.TabIndex = 36;
+            // 
+            // txt5
+            // 
+            this.txt5.Location = new System.Drawing.Point(28, 175);
+            this.txt5.Name = "txt5";
+            this.txt5.Size = new System.Drawing.Size(100, 21);
+            this.txt5.TabIndex = 35;
+            // 
+            // txt4
+            // 
+            this.txt4.Location = new System.Drawing.Point(28, 148);
+            this.txt4.Name = "txt4";
+            this.txt4.Size = new System.Drawing.Size(100, 21);
+            this.txt4.TabIndex = 34;
+            // 
+            // txt8
+            // 
+            this.txt8.Location = new System.Drawing.Point(28, 256);
+            this.txt8.Name = "txt8";
+            this.txt8.Size = new System.Drawing.Size(100, 21);
+            this.txt8.TabIndex = 33;
+            // 
+            // txt3
+            // 
+            this.txt3.Location = new System.Drawing.Point(28, 121);
+            this.txt3.Name = "txt3";
+            this.txt3.Size = new System.Drawing.Size(100, 21);
+            this.txt3.TabIndex = 32;
+            // 
+            // txt2
+            // 
+            this.txt2.Location = new System.Drawing.Point(28, 94);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 21);
+            this.txt2.TabIndex = 31;
+            // 
+            // txt1
+            // 
+            this.txt1.Location = new System.Drawing.Point(28, 67);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 21);
+            this.txt1.TabIndex = 30;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.Location = new System.Drawing.Point(59, 37);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(41, 12);
+            this.lblName.TabIndex = 29;
+            this.lblName.Text = "과목명";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(458, 346);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grd8);
+            this.Controls.Add(this.grd7);
+            this.Controls.Add(this.grd6);
+            this.Controls.Add(this.grd5);
+            this.Controls.Add(this.grd4);
+            this.Controls.Add(this.grd3);
+            this.Controls.Add(this.grd2);
+            this.Controls.Add(this.grd1);
+            this.Controls.Add(this.lblGrade);
+            this.Controls.Add(this.crd8);
+            this.Controls.Add(this.crd7);
+            this.Controls.Add(this.crd6);
+            this.Controls.Add(this.crd5);
+            this.Controls.Add(this.crd4);
+            this.Controls.Add(this.crd3);
+            this.Controls.Add(this.crd2);
+            this.Controls.Add(this.crd1);
+            this.Controls.Add(this.lblCrd);
+            this.Controls.Add(this.txt7);
+            this.Controls.Add(this.txt6);
+            this.Controls.Add(this.txt5);
+            this.Controls.Add(this.txt4);
+            this.Controls.Add(this.txt8);
+            this.Controls.Add(this.txt3);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.lblName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox grd8;
+        private System.Windows.Forms.ComboBox grd7;
+        private System.Windows.Forms.ComboBox grd6;
+        private System.Windows.Forms.ComboBox grd5;
+        private System.Windows.Forms.ComboBox grd4;
+        private System.Windows.Forms.ComboBox grd3;
+        private System.Windows.Forms.ComboBox grd2;
+        private System.Windows.Forms.ComboBox grd1;
+        private System.Windows.Forms.Label lblGrade;
+        private System.Windows.Forms.ComboBox crd8;
+        private System.Windows.Forms.ComboBox crd7;
+        private System.Windows.Forms.ComboBox crd6;
+        private System.Windows.Forms.ComboBox crd5;
+        private System.Windows.Forms.ComboBox crd4;
+        private System.Windows.Forms.ComboBox crd3;
+        private System.Windows.Forms.ComboBox crd2;
+        private System.Windows.Forms.ComboBox crd1;
+        private System.Windows.Forms.Label lblCrd;
+        private System.Windows.Forms.TextBox txt7;
+        private System.Windows.Forms.TextBox txt6;
+        private System.Windows.Forms.TextBox txt5;
+        private System.Windows.Forms.TextBox txt4;
+        private System.Windows.Forms.TextBox txt8;
+        private System.Windows.Forms.TextBox txt3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label lblName;
+    }
+}
+
