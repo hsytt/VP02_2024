535c344c39a1f1600c94849ca683ec89e4eb8de1
hsytt <165130077+hsytt@users.noreply.github.com>
2024-04-03 14:39:55 +0900

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,95 +1,45 @@
-﻿namespace _016_loop
+﻿namespace _017_array
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            // (43장) x에서 y까지 합, 홀수 합, 역수 합
+            int[] a = new int[10];
 
-            // 1에서 100까지의 합
+            for (int i = 0; i < a.Length; i++)
+            {
+                a[i] = int.Parse(Console.ReadLine());
+            }
+            // (1) 원소를 모두 출력하시오
+            // (1)-1 인덱스를 사용하기
+            for (int i = 0; i < 10; i++)
+                Console.Write(a[i] + " ");
+            Console.WriteLine();
+            
+            // (1)-2 foreach 사용
+            foreach (var i in a)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            // (2) 원소의 합을 출력하시오
+            // (2)-1 인덱스를 사용하기
             int sum = 0;
-            for(int i = 1; i <= 100; i++)
-            {
-                sum += i;
-            }
-            Console.WriteLine("1에서 100까지의 합은 {0}", sum);
-
-            // 1에서 100까지의 홀수의 합
+            for(int i=0; i<10; i++)
+                sum += a[i];
+            Console.WriteLine("합 = " + sum);
+            
+            // (2)-2 foreach 사용
             int sum2 = 0;
-            for(int i = 1; i <= 100; i++)
+            foreach ( var value in a)
             {
-                if (i % 2 == 1)
-                    sum2 += i;
+                sum2 += value;
             }
-            Console.WriteLine("1에서 100까지의 홀수의 합은 {0}", sum2);
-
-
-            // 1에서 100까지의 역수의 합
-            double sum3 = 0;
-            for (int i = 1; i <= 100; i++)
-            {
-                sum3 += 1.0 / i;
-             
-            }
-            Console.WriteLine("1에서 100까지의 역수의 합 = " + sum3);
-
-
-            // x에서 y까지의 합
-            int sum4 = 0;
-            Console.Write("x :");
-            int x = int.Parse(Console.ReadLine());
-            Console.Write("y :");
-            int y = int.Parse(Console.ReadLine());
-            for(int i = x; i <= y; i++)
-            {
-                sum4 += i;
-            }
-            Console.WriteLine("{0}~{1}까지의 합 {2}", x, y, sum4);
-
-            //x에서 y까지의 홀수의 합
-            int sum5 = 0;
-            Console.Write("x1 :");
-            int x1 = int.Parse(Console.ReadLine());
-            Console.Write("y1 :");
-            int y1 = int.Parse(Console.ReadLine());
-            for (int i = x1; i <= y1; i++)
-            {
-                if (i % 2 == 1)
-                    sum5 += i;  
-            }
-            Console.WriteLine("{0}~{1}까지의 홀수의 합 {2}", x1, y1, sum5);
-
-            // x에서 y까지의 역수의 합
-            double sum6 = 0;
-            Console.Write("x2 :");
-            int x2 = int.Parse(Console.ReadLine());
-            Console.Write("y2 :");
-            int y2 = int.Parse(Console.ReadLine());
-            for (int i = x2; i <= y2; i++)
-            {
-                sum6 += 1.0 / i;
-            }
-            Console.WriteLine("{0}~{1}까지의 합 {2}", x, y, sum6);
-
-            // (45장) 구구단 
-
-            // (47장) a의 b승
-            Console.Write("a를 입력하세요 :");
-            int a = int.Parse(Console.ReadLine());
-            Console.Write("b를 입력하세요 :");
-            int b = int.Parse(Console.ReadLine());
-
-            int pow = 1;
-            for (int i = 0; i < b; i++)
-            {
-                pow *= a;
-            }
-            Console.WriteLine("{0}의 {1}승의 값은 {2}입니다.", a, b, pow);
-                
-
-
-
-            // (53장) 팩토리얼
+            Console.WriteLine("합 = " + sum2);
+            // (3) 가장 큰 값을 출력하시오
+            int max = a[0];
+            for (int i = 1; i < 10; i++)
+                if (max < a[i])
+                    max = a[i];
+            Console.WriteLine("최대값 = " + max);
         }
     }
 }
