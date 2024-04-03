6bbbadd6849c04106db670655a3d2160269bc11a
hsytt <165130077+hsytt@users.noreply.github.com>
2024-04-03 15:41:42 +0900

    Add files via upload

---
+++ 017_array/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace _017_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new int[10];
+
+           // for (int i = 0; i < a.Length; i++)
+           //{
+           //     a[i] = int.Parse(Console.ReadLine());
+           // }
+
+            // 배열에 10개의 랜덤값을 저장하시오
+            Random r = new Random();
+            for(int i=0; i<10; i++)
+                a[i] = r.Next(1000);
+
+
+            // (1) 원소를 모두 출력하시오
+            // (1)-1 인덱스를 사용하기
+            for (int i = 0; i < 10; i++)
+                Console.Write(a[i] + " ");
+            Console.WriteLine();
+            
+            // (1)-2 foreach 사용
+            foreach (var i in a)
+                Console.Write(i + " ");
+            Console.WriteLine();
+            // (2) 원소의 합을 출력하시오
+            // (2)-1 인덱스를 사용하기
+            int sum = 0;
+            for(int i=0; i<10; i++)
+                sum += a[i];
+            Console.WriteLine("합 = " + sum);
+            
+            // (2)-2 foreach 사용
+            int sum2 = 0;
+            foreach ( var value in a)
+            {
+                sum2 += value;
+            }
+            Console.WriteLine("합 = " + sum2);
+            // (3) 가장 큰 값을 출력하시오
+            int max = a[0];
+            for (int i = 1; i < 10; i++)
+                if (max < a[i])
+                    max = a[i];
+            Console.WriteLine("최대값 = " + max);
+        }  
+    }
+}   
---
+++ 018_Chart1/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _018_Chart1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // 차트 컨드롤을 사용하여 랜덤하게 0에서 100사이의 값 10개를 그래프로 그려라
+            Random r = new Random();
+            for (int i = 0; i<10; i++)
+            {
+                chart1.Series[0].Points.Add(r.Next(101));
+            }
+            chart1.Series[0].LegendText = "비주얼\n프로그래밍";
+            chart1.Titles[0].Text = "성적";
+        }
+    }
+}
